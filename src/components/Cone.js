776e692e2a1b39a0a8ec22ce48d0ba783a54663d
Siddartha <siddartharedddyl@gmail.com>
2023-11-09 09:41:43 +0530

    Added events to Cone.js

---
+++ src/components/Cone.js
@@ -0,0 +1,12 @@
+let Cone=()=>{
+    const ClickHandle=()=>{
+        alert("Siddartha clicked button")
+    }
+    return(
+        <>
+        <h1>Siddartha</h1>
+        <button onClick={ClickHandle}>Press Here</button>
+        </>
+    );
+};
+export default Cone;
